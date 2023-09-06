b205548c0e53ca128eab4b8d84c29dc4285a9b85
Pedro Gomes <122326524+PedroGomees@users.noreply.github.com>
2023-09-06 11:32:50 -0300

    Upgrade the game

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 var total = 0;
+var intervalo = setInterval(criarBolhas, 500);
+var estaPausado = false;
+var estourarAtivo = true;
+
+
+
 function criarBolhas(){
+  let pauseBTN = document.getElementById('pause').addEventListener('click', pauseGame);
+  let playBTN = document.getElementById('play').addEventListener('click', playGame);
+  
 
-
-
-    var bolha = document.createElement('span');
-   
+  var bolha = document.createElement("span");
+    
 
     var x = Math.floor(Math.random()* 1800);
     var y = Math.floor(Math.random()* 800);
@@ -18,17 +25,51 @@ function criarBolhas(){
     bolha.style.top = y+ "px";
     document.body.appendChild(bolha);
     
+    function playGame(){
+
+      bolha.style.animation = "animate 7s ease-in-out infinite"
+
+      if(estaPausado){
+        intervalo = setInterval(criarBolhas, 500);
+        estaPausado = false;
+        estourarAtivo = true;
+       
+      }
+      
+    }
+    function pauseGame(){
+      
+      bolha.style.animation = "none";
+      clearInterval(intervalo);
+      estaPausado = true;
+      estourarAtivo = false;
+     
+    }
 }
 
-function estourar(objeto){
 
-    var container = document.getElementById("game");
+
+
+
+function estourar(objeto){
+ 
+  if(estourarAtivo){
+    var container = document.getElementById("bg");
 
     
     total++;
-    var score = document.getElementById("bg");
+    var score = document.getElementById("game");
     document.body.removeChild(objeto);
-    bg.innerHTML = `<p>${total}</p>`
-  }
+    var number = document.querySelector('p');
+    score.innerHTML = `<p>${total}</p>`
+    upScore();
+    upScore(number);
+    number.style.position = "absolute";
+  }}
 
-setInterval(criarBolhas, 500);
+  function upScore(){
+    var score = document.getElementById('game');
+    var escala = 1 + total ;
+    var fontsize = `${escala * 4}px`;
+    score.style.fontSize = fontsize;
+  }
